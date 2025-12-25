f83c814c717a137628c06dc2006117770bc9c9e4
Fantasy147 <fantasylife147@gmail.com>
2025-12-25 22:42:55 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const API = "https://SEU_BACKEND/api";
+
+fetch(`${API}/dramas.php`)
+  .then(r => r.json())
+  .then(data => {
+    const main = document.getElementById("dramas");
+    data.forEach(d => {
+      main.innerHTML += `
+        <div class="card">
+          <img src="${d.cover}">
+          <h3>${d.title}</h3>
+        </div>`;
+    });
+  });
+
+function openLogin() {
+  document.getElementById("loginModal").style.display = "block";
+}
+function closeLogin() {
+  document.getElementById("loginModal").style.display = "none";
+}
+
+function login() {
+  fetch(`${API}/login.php`, {
+    method: "POST",
+    body: JSON.stringify({
+      email: email.value,
+      password: password.value
+    })
+  })
+  .then(r => r.json())
+  .then(d => alert("Bem-vindo " + d.users))
+  .catch(() => alert("Erro de login"));
+}
